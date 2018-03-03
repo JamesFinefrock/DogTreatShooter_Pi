9b8ca8cb524dca98ad9a190362445846f50f2bca
James Finefrock <finefrockjames@gmail.com>
2018-03-03 15:03:46 -0500

    Bluetooth read test

--- connect_ble.py
+++ connect_ble.py
@@ -1,11 +1,41 @@
-from bluetooth.ble import BeaconService
-import time
+#!/usr/bin/python
+# -*- mode: python; coding: utf-8 -*-
 
-service = BeaconService()
+# Copyright (C) 2014, Oscar Acena <oscaracena@gmail.com>
+# This software is under the terms of GPLv3 or later.
 
-service.start_advertising("11111111-2222-3333-4444-555555555555",
-                    1, 1, 1, 200)
-time.sleep(15)
-service.stop_advertising()
+from __future__ import print_function
 
-print("Done.")
+import sys
+from bluetooth.ble import GATTRequester
+
+
+class Reader(object):
+    def __init__(self, address):
+        self.requester = GATTRequester(address, False)
+        self.connect()
+        self.request_data()
+
+    def connect(self):
+        print("Connecting...", end=' ')
+        sys.stdout.flush()
+
+        self.requester.connect(True)
+        print("OK!")
+
+    def request_data(self):
+        data = self.requester.read_by_uuid(
+                "00002a00-0000-1000-8000-00805f9b34fb")[0]
+        try:
+            print("Device name: " + data.decode("utf-8"))
+        except AttributeError:
+            print("Device name: " + data)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: {} <addr>".format(sys.argv[0]))
+        sys.exit(1)
+
+    Reader(sys.argv[1])
+    print("Done.")
