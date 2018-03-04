fa8a57fa19619fc893c36a99364e66a955f8b735
Josh Miller <joshuadmiller98@gmail.com>
2018-03-03 21:10:06 -0500

    fixed things

--- treatShooter.py
+++ treatShooter.py
@@ -5,13 +5,14 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 
 p = GPIO.PWM(7,50)
-p.start(7.5)
+p.start(0)
 
 
 time.sleep(2)
-p.ChangeDutyCycle(50)
+p.ChangeDutyCycle(25)
 time.sleep(2)
-p.ChangeDutyCycle(7.5)
+p.ChangeDutyCycle(0)
+time.sleep(2)
   
 
 GPIO.cleanup()
