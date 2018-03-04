7b0ce7f513ada8569743514afd6cb2adb9d9b237
Josh Miller <joshuadmiller98@gmail.com>
2018-03-03 22:16:01 -0500

    Update treatShooter.py

--- treatShooter.py
+++ treatShooter.py
@@ -9,10 +9,10 @@ p.start(0)
 
 
 time.sleep(1)
-p.ChangeDutyCycle(7.5)
+p.ChangeDutyCycle(5.5)
 time.sleep(.5)
-#p.ChangeDutyCycle(0)
-#time.sleep(2)
+p.ChangeDutyCycle(0)
+time.sleep(2)
 
 p.stop()
 
